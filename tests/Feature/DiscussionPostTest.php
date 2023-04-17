e80950b751555fc28ab8d3705a09182e5d6dc842
Bobby Iliev <bobby@bobbyiliev.com>
2023-04-17 09:07:09 +0000

    Add more tests

---
+++ tests/Feature/DiscussionPostTest.php
@@ -0,0 +1,101 @@
+<?php
+
+use function Pest\Livewire\livewire;
+
+use FoundationApp\Discussions\Models\Discussion as DiscussionModel;
+use FoundationApp\Discussions\Components\Discussion;
+use FoundationApp\Discussions\Components\Discussions;
+use FoundationApp\Discussions\Components\Posts;
+
+test('list_and_create_discussion_posts', function () {
+    $user = user('test1');
+
+    $this->actingAs($user);
+
+    livewire(Discussions::class, ['user' => $user])
+        ->set('title', 'Test first post')
+        ->set('content', 'Test first post')
+        ->call('createDiscussion')
+        ->assertSee('Discussion created successfully.');
+    $discussion = DiscussionModel::where('title', 'Test first post')->first();
+    $this->assertNotNull($discussion);
+});
+
+test('edit_delete_discussion_posts', function () {
+    $user = user('test1');
+
+    $this->actingAs($user);
+
+    livewire(Discussions::class, ['user' => $user])
+        ->set('title', 'Test first post')
+        ->set('content', 'Test first post')
+        ->call('createDiscussion')
+        ->assertSee('Discussion created successfully.');
+    $discussion = DiscussionModel::where('title', 'Test first post')->first();
+
+    livewire(Discussion::class, ['discussion_slug' => $discussion->slug])
+        ->assertSee('Test first post')
+        ->assertSee('Test first post');
+
+    livewire(Discussion::class, ['discussion_slug' => $discussion->slug])
+        ->set('comment', 'Test first post answer')
+        ->call('answer');
+
+    livewire(Discussion::class, ['discussion_slug' => $discussion->slug])
+        ->set('comment', 'Test first post answer 2')
+        ->call('answer');
+
+    $answer = $discussion->posts()->where('content', 'Test first post answer')->first();
+
+    livewire(Posts::class, ['discussion' => $discussion])
+        ->assertSee('Test first post answer')
+        ->assertSee('Test first post');
+
+    // edit the post
+    livewire(Posts::class, ['discussion' => $discussion])
+        ->call('edit', $answer->id)
+        ->set('editedContent', 'Test first post answer edited')
+        ->call('update', $answer->id)
+        ->assertSee('Test first post answer edited')
+        ->assertSee('Test first post');
+
+    // delete the post
+    livewire(Posts::class, ['discussion' => $discussion])
+        ->call('delete', $answer->id)
+        ->assertDontSee('Test first post answer edited')
+        ->assertSee('Test first post');
+});
+
+test('edit_delete_posts_as_another_user', function () {
+
+    $user = user('test1');
+
+    $this->actingAs($user);
+
+    livewire(Discussions::class, ['user' => $user])
+        ->set('title', 'Test first post')
+        ->set('content', 'Test first post')
+        ->call('createDiscussion')
+        ->assertSee('Discussion created successfully.');
+    $discussion = DiscussionModel::where('title', 'Test first post')->first();
+    livewire(Discussion::class, ['discussion_slug' => $discussion->slug])
+        ->set('comment', 'Test first post answer')
+        ->call('answer');
+
+    $answer = $discussion->posts()->where('content', 'Test first post answer')->first();
+
+    $user2 = user('test2');
+
+    $this->actingAs($user2);
+
+    livewire(Posts::class, ['discussion' => $discussion])
+        ->call('edit', $answer->id)
+        ->set('editedContent', 'Test first post answer edited')
+        ->call('update', $answer->id)
+        ->assertSee('Test first post answer')
+        ->assertDontSee('Test first post answer edited');
+
+    livewire(Posts::class, ['discussion' => $discussion])
+        ->call('delete', $answer->id)
+        ->assertSee('Test first post answer');
+});
